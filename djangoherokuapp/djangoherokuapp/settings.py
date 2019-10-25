a0690fbc63694b9b4b648a398c09e669d4cc8ebf
kj21012 <kjyeboah21012@gmail.com>
2019-10-24 23:46:51 -0700

    Fix settings

--- djangoherokuapp/djangoherokuapp/settings.py
+++ djangoherokuapp/djangoherokuapp/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'herokuapp',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
